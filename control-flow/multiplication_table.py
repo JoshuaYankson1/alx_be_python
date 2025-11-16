35e3e708185af9e6e42657c0c5ff83d20031f3ca
JoshuaYankson1 <joshuayamoah2016@gmail.com>
2025-11-16 06:00:56 +0000

    fixed the "x" sign to "*" sign to match the regex pattern of the checker

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -10,4 +10,4 @@ number = int(input('Enter a number to see its multiplication table: '))
 
 for num in range(1, 11):
     result = num * number
-    print(f'{number} x {num} = {result}')
+    print(f'{number} * {num} = {result}')
