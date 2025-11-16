682a43989da016a7d142e238107836ead09ebd7f
JoshuaYankson1 <joshuayamoah2016@gmail.com>
2025-11-16 05:57:17 +0000

    a program to print the multiplication table based on user input

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,13 @@
+#This code prompts the user to enter a number and then prints the multiplication table for that number from 1 to 10.
+ # Example usage:
+# If the user inputs 5, the output will be:
+# 5 x 1 = 5
+# 5 x 2 = 10
+
+
+
+number = int(input('Enter a number to see its multiplication table: '))
+
+for num in range(1, 11):
+    result = num * number
+    print(f'{number} x {num} = {result}')
