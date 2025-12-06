0a16e5a22ad1c1d46d2c02f449dc84ad9e313200
JoshuaYankson1 <joshuayamoah2016@gmail.com>
2025-12-06 11:58:59 +0000

    a function that calculates temperature based on two options

---
+++ fns_and_dsa/temp_conversion_tool.py
@@ -0,0 +1,21 @@
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
+
+def convert_to_celsius(fahrenheit):
+    return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
+
+def convert_to_fahrenheit(celsius):
+    return celsius * CELSIUS_TO_FAHRENHEIT_FACTOR + 32
+
+
+user_input = float(input('Enter the temperature to convert: '))
+temperature_type = input('Is this temperature in Celsius or Fahrenheit? (C/F): ').strip().upper()
+
+if temperature_type == 'C':
+    converted_temp = convert_to_fahrenheit(user_input)
+    print(f'{user_input}°C is {converted_temp:.2f}°F')
+elif temperature_type == 'F':
+    converted_temp = convert_to_celsius(user_input)
+    print(f'{user_input}°F is {converted_temp:.2f}°C')
+else:
+    print('Invalid temperature. Please enter a numeric value.')
